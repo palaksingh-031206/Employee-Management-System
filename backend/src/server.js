c31654781e9f21b942f680e741f27aa9062bc20b
Palak Singh <chmcs2026.palak.singh@gmail.com>
2026-03-05 19:42:09 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,6 @@ const port=process.env.PORT || 3001;
 app.use (cors({
         //origin:'http://localhost:5173'
         origin:"https://employee-management-system-1ly3.onrender.com"
-        credentials: true
     }
 ))
 
