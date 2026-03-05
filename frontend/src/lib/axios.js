0df6693cd07bfb7ce10744c82a07b9ee78bd95bf
Palak Singh <chmcs2026.palak.singh@gmail.com>
2026-03-05 19:13:08 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/employees"
+  //baseURL: "http://localhost:3000/employees"
+  //baseURL: "https://employee-management-system-1ly3.onrender.com"
 });
 
 export default api;
