53e53c957a7f0423d935292c96c8403f55b37ef8
Palak Singh <chmcs2026.palak.singh@gmail.com>
2026-03-05 21:54:12 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const port=process.env.PORT || 3001;
 
 app.use (cors({
         //origin:'http://localhost:5173'
-        origin:"https://employee-management-rsdk.onrender.com"
+        
     }
 ))
 
